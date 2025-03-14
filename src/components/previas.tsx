c7366777b52966c44bc811f5d1d98e6c260039f6
gioerramun <gianfranco.erramun@gmail.com>
2025-03-14 19:27:07 -0300

    UPDATED index and computacion

--- src/components/previas.tsx
+++ src/components/previas.tsx
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import computacion from "../data/computacion.json";
 
 //------------INTERFACE PARA MATERIAS
-interface Subject {
-	name: string;
+interface Options {
 	previasExam: string[];
 	previasApproved: string[];
+  }
+
+
+interface Subject {
+	name: string;
+	semester: number;
+	previasExam: string[];
+	previasApproved: string[];
+	options?: Options[];
 }
 
 //------------BUTTON COMPONENT
@@ -28,7 +36,7 @@ const Button: React.FC<ButtonProps> = ({ subject, status, handleToggle, disabled
 		<button
 			onClick={handleClick}
 			disabled={disabled}
-			className={`border p-2 text-black transition-all ${
+			className={`border rounded-lg p-2 text-black font-[700] transition-all ${
 				disabled
 					? "bg-gray-300"
 					: status === "nothing" ? "bg-red-200" : status === "course" ? "bg-yellow-200" : "bg-green-200"}`}>
@@ -57,27 +65,36 @@ const Previas: React.FC = () => {
 	const isEnabled = (subject: Subject) => {
 		return (
 		  subject.previasExam.every((prev) => statuses[prev] === "exam") &&
-		  subject.previasApproved.every((prev) => ["course", "exam"].includes(statuses[prev]))
+		  subject.previasApproved.every((prev) => ["course", "exam"].includes(statuses[prev])) &&
+		  (!subject.options || subject.options.some(
+			(option) =>
+			  option.previasExam.every((prev) => statuses[prev] === "exam") &&
+			  option.previasApproved.every((prev) => ["course", "exam"].includes(statuses[prev]))
+		  ))
 		);
 	  };
-	
 
 	return (
 		<div>
-			<h1>Lista de Materias</h1>
-			<div className="flex flex-col gap-2">
-				{subjects.map((subject) => (
-					<Button
-						key={subject.name}
-						subject={subject}
-						status={statuses[subject.name] || "nothing"}
-						handleToggle={handleToggle}
-						disabled={!isEnabled(subject)}
-					/>
-				))}
+			<div className="border flex flex-col gap-2">
+			{[...Array(8).keys()].map((i) => (
+        		<div key={i + 1} id={(i + 1).toString()} className="border rounded-2xl p-5 w-full flex flex-wrap gap-5 items-center justify-center">
+          		{subjects
+            		.filter((subject) => subject.semester === i + 1)
+            		.map((subject) => (
+              			<Button
+                			key={subject.name}
+               				subject={subject}
+                			status={statuses[subject.name] || "nothing"}
+                			handleToggle={handleToggle}
+                			disabled={!isEnabled(subject)}
+              			/>))}
+        		</div>
+      		))}
 			</div>
 		</div>
 	);
 };
 
 export default Previas;
+
